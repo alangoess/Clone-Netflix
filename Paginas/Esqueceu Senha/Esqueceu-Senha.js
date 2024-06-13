9ca6d149ac8455fe8c7c09244f9e259f22974c9a
Alan <depzz123@gmail.com>
2024-06-13 15:52:49 -0300

    Modificações nas paginas de criar conta, e esqueceu senha

--- Paginas/Esqueceu Senha/Esqueceu-Senha.js
+++ Paginas/Esqueceu Senha/Esqueceu-Senha.js
@@ -1,3 +1,18 @@
 function EsqueceuSenha(link){
 location.href = './Esqueceu-Senha.html'
 }
+
+document.addEventListener('click', function() {
+    let verificarEmail = document.getElementById('verificar-email');
+    let telefone = document.getElementById('telefone');
+    let info = document.getElementById('msg-send-email');
+
+    if (verificarEmail.checked) {
+        info.innerHTML = 'Enviaremos um email com instruções de como redefinir sua senha.';
+    }else{
+        info.innerHTML = 'Enviaremos uma mensagem de texto com um código de redefinição da sua senha. Sujeito a tarifas sobre o uso de dados e mensagens.'
+    }
+});
+
+
+
