08a9925c93ce2bc5bed7f2a947ce8c1d695ac867
Alan <depzz123@gmail.com>
2024-06-20 16:58:01 -0300

    Pagina de cadastro a partir do plano promocional acrescentada

---
+++ Paginas/sign-up/ads-cadastro/ads-cadastro.js
@@ -0,0 +1,17 @@
+const btn_entrar = document.getElementById('btn-entrar')
+
+btn_entrar.addEventListener('click', function(){
+    window.location.href = './Paginas/Login/index.html';
+})
+
+const btn = document.getElementById('btn-question1')
+const modal = document.querySelector('dialog')
+const btn_close = document.getElementById('closeModal')
+
+btn.onclick = function(){
+    modal.show()
+}
+
+btn_close.onclick = function(){
+    modal.close()
+}
--- Paginas/sign-up/sign-up.js
+++ Paginas/sign-up/sign-up.js
@@ -7,18 +7,13 @@ btn_entrar.addEventListener('click', function(){
 const btn = document.getElementById('btn-question1')
 const modal = document.querySelector('dialog')
 const btn_close = document.getElementById('closeModal')
+const promo_assinar = document.getElementById('promo_assinar')
+const btn_assinar = document.getElementById('btn-assinar')
 
+promo_assinar.addEventListener('click', function(){
+    location.href = './ads-cadastro/ads-cadastro.html'
+})
 
- const btn_assinar = document.getElementById('btn-assinar')
  btn_assinar.addEventListener('click', function(){
     window.location.href = './terminar_cadastro/sign-up.html'
  })
-
-
-btn.onclick = function(){
-    modal.show()
-}
-
-btn_close.onclick = function(){
-    modal.close()
-}
