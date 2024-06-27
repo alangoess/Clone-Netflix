049f12b98ea2155a5e59dfe2e32f00861c576526
Alan <depzz123@gmail.com>
2024-06-27 14:19:31 -0300

    Atualizacoes de paginas

--- Paginas/criar-usuario/escolher_plano2/sign-up.js
+++ Paginas/criar-usuario/escolher_plano2/sign-up.js
@@ -1,5 +1,5 @@
 const btn_proximo = document.getElementById('btn-proximo')
 
 btn_proximo.addEventListener('click', function(){
-    window.location.href = '../../criar-usuario/escolher_plano2/sign-up.html';
+    window.location.href = '../../criar-usuario/ads-cadastro.html';
 })
